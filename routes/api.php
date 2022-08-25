bbcdc5e50ee03a69be45e4ee3fbda00df7b130f5
isotop786 <marufislam77@gmail.com>
2022-08-25 12:57:31 +0600

    Register init

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//////////////////// Admin //////////////////////
+Route::prefix('admin')->group(function(){
+    Route::post('register',[AuthController::class,'register']);
+});
+
+
+
+
+///////////////// Ambassador ///////////////////
+
+
+
+
+
+/////////////// Checkout /////////////////////
